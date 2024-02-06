030c14818b456c148d194a981030e11c1ef39bcf
alzneo <25908453+alzneo@users.noreply.github.com>
2024-02-06 13:03:07 +0500

    language selection fix

--- app/embedWww/lang.js
+++ app/embedWww/lang.js
@@ -1,5 +1,5 @@
 var languages = {
-    "ru-RU": {
+    "ru": {
         rebootBtn: "Перезагрузить",
         shutdownBtn: "Выключить",
         cancelMsg: "Есть около минуты, чтобы передумать",
@@ -13,6 +13,6 @@ var languages = {
 
 document.querySelectorAll('[id]').forEach(elem => {
     var id = elem.id;
-    var lang = navigator.language;
+    var lang = navigator.language.substring(0, 2);
     elem.innerHTML = languages[lang][id];
 });
